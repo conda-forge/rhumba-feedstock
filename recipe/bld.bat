b4a71f9b63cbcb532ec290d9eab2f15c0c729492
Uwe L. Korn <uwe.korn@quantco.com>
2020-10-09 14:07:06 +0200

    Enable C++17

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,1 +1,7 @@
+rem Enforce C++17
+cp %RECIPE_DIR%/configure.win .
+IF %ERRORLEVEL% NEQ 0 exit 1
+sed -i s/std=c++14/std=c++17/g %R_MAKEVARS_SITE%
+IF %ERRORLEVEL% NEQ 0 exit 1
 "%R%" CMD INSTALL --build .
+IF %ERRORLEVEL% NEQ 0 exit 1
