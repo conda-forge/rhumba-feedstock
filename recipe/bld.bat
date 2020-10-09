e133fda626548c62396e27ced3b6cfcd6957e121
Uwe L. Korn <uwe.korn@quantco.com>
2020-10-09 16:21:09 +0200

    s/cp/copy/

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
 rem Enforce C++17
-cp %RECIPE_DIR%/configure.win .
+copy %RECIPE_DIR%/configure.win .
 IF %ERRORLEVEL% NEQ 0 exit 1
 sed -i s/std=c++14/std=c++17/g %R_MAKEVARS_SITE%
 IF %ERRORLEVEL% NEQ 0 exit 1
